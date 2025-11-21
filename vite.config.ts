fb6830608e56a2d46abd9d3b1ba37dec78262f80
Michele Picchioni <michelepicchioni@MacBook-Pro-di-Michele.local>
2025-11-21 11:24:47 +0000

    Configure for custom domain thebricksfactory.com

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Se usi un dominio personalizzato (thebricksfactory.com), la base deve essere '/'
+  // Se invece usi github pages senza dominio (username.github.io/repo), allora serve '/repo-name/'
+  base: '/',
 })
